0da1cda1cffc1edfd15bfd6316c641058d198262
Samuel McBroom <sam@sammcb.com>
2020-01-19 20:13:49 -0500

    Steps is now an int.

--- colorsfinder/colorsfinder.js
+++ colorsfinder/colorsfinder.js
@@ -5,7 +5,7 @@ function findColors() {
     alert('Must choose different colors');
     return;
   }
-  steps = document.getElementById('steps').value;
+  steps = parseInt(document.getElementById('steps').value);
   document.getElementById('content').style.backgroundImage = 'linear-gradient(' + c1 + ', ' + c2 + ')';
   result = document.getElementById('result');
   result.innerHTML = '';
@@ -13,6 +13,7 @@ function findColors() {
   rgb1 = [parseInt('0x' + c1[1] + c1[2]), parseInt('0x' + c1[3] + c1[4]), parseInt('0x' + c1[5] + c1[6])];
   rgb2 = [parseInt('0x' + c2[1] + c2[2]), parseInt('0x' + c2[3] + c2[4]), parseInt('0x' + c2[5] + c2[6])];
   delta = [rgb1[0] - rgb2[0], rgb1[1] - rgb2[1], rgb1[2] - rgb2[2]];
+  console.log(delta);
 
   prevRgb = [rgb1[0], rgb1[1], rgb1[2]];
   resColors = [c1];
@@ -22,6 +23,7 @@ function findColors() {
       Math.round(rgb1[1] - (i * (delta[1] / (steps + 1)))),
       Math.round(rgb1[2] - (i * (delta[2] / (steps + 1))))
     ];
+    console.log(newRgb);
 
     if (newRgb[0] === prevRgb[0] && newRgb[1] === prevRgb[1] && newRgb[2] === prevRgb[2]) continue;
 
